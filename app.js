caaab8b1104d0ae93fd76cbbdd56b65f4d374a06
Abbi Parchment <abbigail151@gmail.com>
2021-10-31 18:51:20 -0500

    Completed Exercise Three (3)

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-window.onload = function(){
+window.onload = function() { 
 
     var Sbutton = document.getElementById("btn");
-    Sbutton.addEventListener("click", function() {
-        fetch("http://localhost:8888/info2180-lab4/superheroes.php")
-        .then(response => response.text())
-        .then(data => {
-           alert(data) 
-        });
+    Sbutton.addEventListener("click", function(e) {
+        e.preventDefault();
+
+
+    var result = document.getElementById("result");
+    var valInput = document.getElementById("superhero").value.replace(/[-&\/\\#,+()$@|~%!.'":;*?<>{}]/g,'');
+    
+    fetch(`http://localhost:8888/info2180-lab4/superheroes.php?query= ${valInput}`, {method : 'GET'})
+    .then(response => response.text())
+    .then(data => {
+        result.innerHTML = "";
+        result.innerHTML = data; 
     });
+  });
 };
+
 
 
